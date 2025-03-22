b1c6c7a932b5ad547ce7a9ce2a0d92132c08f56d
anku666 <ankit.zagade@gmail.com>
2025-03-23 01:22:26 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,6 @@ class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World new world");
+        System.out.println("Hello, World new world 2 c world");
     }
 }
